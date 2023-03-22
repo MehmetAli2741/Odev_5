202298d0140ba48de564ad61c046a2955f995c5d
Mehmet Ali Yilmaz <mehmetaliyilmaz2741@outlook.com>
2023-03-22 14:47:11 +0300

    postgresql ode5 tamamlandı

---
+++ customer_storeid_lastname_limit5.sql
@@ -0,0 +1,16 @@
+--SELECT * FROM film
+--WHERE  title LIKE '%n'
+--ORDER BY length DESC
+--LIMIT 5;
+
+--SELECT * FROM film
+--WHERE  title LIKE '%n'
+--ORDER BY length
+--OFFSET 5
+--LIMIT 5;
+
+SELECT * FROM customer
+ORDER BY store_id,last_name DESC
+LIMIT 5;
+
+
---
+++ film_titile_n_length_Asc_offset5_lımit5.sql
@@ -0,0 +1,10 @@
+--SELECT * FROM film
+--WHERE  title LIKE '%n'
+--ORDER BY length DESC
+--LIMIT 5;
+
+SELECT * FROM film
+WHERE  title LIKE '%n'
+ORDER BY length
+OFFSET 5
+LIMIT 5;
